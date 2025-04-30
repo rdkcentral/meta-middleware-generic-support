bd573b262465060d54dfe562526c5d02ea321c51
Rajat <Rajat_Yadav2@comcast.com>
2025-04-30 12:54:40 -0400

    VPLAY-9326  OSX AAMP simulator build failure after OSX updates
    Reason for change: To update the correct version of openssl to install in the install-aamp script
    Risks: Low
    Priority: P0
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_25_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_25_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_25_1
    
    Change-Id: I6dc8b1e82a8d66853eef3397762d9c3ce79c6820
    Signed-off-by: Rajat <Rajat_Yadav2@comcast.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -198,7 +198,7 @@ function install_pkgs_fn()
           brew update
       fi
 
-      install_pkgs_darwin_fn git json-glib cmake "openssl@3.4" libxml2 ossp-uuid cjson gnu-sed jpeg-turbo taglib speex mpg123 meson ninja pkg-config flac asio jsoncpp lcov gcovr jq curl
+      install_pkgs_darwin_fn git json-glib cmake "openssl@3" libxml2 ossp-uuid cjson gnu-sed jpeg-turbo taglib speex mpg123 meson ninja pkg-config flac asio jsoncpp lcov gcovr jq curl
       install_pkgs_darwin_fn coreutils websocketpp "boost@1.85" jansson libxkbcommon cppunit gnu-sed fontconfig doxygen graphviz tinyxml2 openldap krb5
 
       # ORC causes compile errors on x86_64 Mac, but not on ARM64
