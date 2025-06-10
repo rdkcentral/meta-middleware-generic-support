rdkcentral/aamp

RDK-56217 Decouple DRM License dependencies in AAMP
Reason for change: Decouple License dependencies to seperate folder.
Test Procedure: Refer Ticket
Risks: Low
Priority: P1

Change-Id: I8e53a1159dbdd2d862d4850b02f687e4f06b1081
Signed-off-by: Deepikasri N <Deepikasri_N@Comcast.Com

test/utests/mocks/MockAampLicManager.h
@@ -17,10 +17,18 @@
 * limitations under the License.
 */
 
-#include "DrmSessionFactory.h"
+#ifndef AAMP_MOCK_AAMP_LICENSE_MANAGER_H
+#define AAMP_MOCK_AAMP_LICENSE_MANAGER_H
 
-DrmSession* DrmSessionFactory::GetDrmSession(DrmHelperPtr drmHelper, DrmCallbacks *drmCallbacks)
+#include <gmock/gmock.h>
+#include "AampLicManager.h"
+
+class MockAampLicenseManager
 {
-	DrmSession *safe_value = nullptr;
-	return safe_value;
-}
+public:
+    MOCK_METHOD(void, setVideoWindowSize, (int width, int height));
+};
+
+extern MockAampLicenseManager *g_mockAampLicenseManager;
+
+#endif /* AAMP_MOCK_AAMP_LICENSE_MANAGER_H */

rdkcentral/meta-middleware-generic-support

Change-Id: I38c93ca222ccd2dfc594ce66858f07e659d2dc4b

test/utests/mocks/MockAampLicManager.h
@@ -17,10 +17,18 @@
 * limitations under the License.
 */
 
-#include "DrmSessionFactory.h"
+#ifndef AAMP_MOCK_AAMP_LICENSE_MANAGER_H
+#define AAMP_MOCK_AAMP_LICENSE_MANAGER_H
 
-DrmSession* DrmSessionFactory::GetDrmSession(DrmHelperPtr drmHelper, DrmCallbacks *drmCallbacks)
+#include <gmock/gmock.h>
+#include "AampDRMLicManager.h"
+
+class MockAampLicenseManager
 {
-	DrmSession *safe_value = nullptr;
-	return safe_value;
-}
+public:
+    MOCK_METHOD(void, setVideoWindowSize, (int width, int height));
+};
+
+extern MockAampLicenseManager *g_mockAampLicenseManager;
+
+#endif /* AAMP_MOCK_AAMP_LICENSE_MANAGER_H */